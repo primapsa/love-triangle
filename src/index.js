ab91aa0e7927453b006f61027ee33094ace2d75e
Siarhei Prymakovich <prima.psa@gmail.com>
2019-03-14 16:22:28 +0300

    feat: add triangle counting function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+var count=0;
+for (var n=0; n<preferences.length; n++){   
+  if(preferences[n]!==(n+1)){
+    if(preferences[preferences[preferences[n]-1]-1]===(n+1)){
+        count++;       
+      }
+  } 
+}  
+  return (count/3);
+}
+ 
